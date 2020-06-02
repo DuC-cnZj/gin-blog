3cccb0331380d833b94019d86f387846ad16f457
duc <1025434218@qq.com>
2020-06-03 01:49:44 +0800

    Revert "wip"
    This reverts commit e0bbe016b26b8ff3604cdfa4d0756159f0acd961.

--- routes/web.go
+++ routes/web.go
@@ -136,8 +136,8 @@ const temp = `
 登陆中...
 <script>
     window.onload = function () {
-        window.top.opener.postMessage("bearer {{ .token }}", "{{ .domain }}");
-        window.top.close();
+        window.opener.postMessage("bearer {{ .token }}", "{{ .domain }}");
+        window.close();
     }
 </script>
 </body>
